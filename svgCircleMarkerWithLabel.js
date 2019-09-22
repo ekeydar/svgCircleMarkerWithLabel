cf9ff8001d27953e27dc0dbe87689ca434d697c4
Eran Keydar <ekeydar@gmail.com>
2019-09-22 22:20:48 +0300

    ident

--- svgCircleMarkerWithLabel.js
+++ svgCircleMarkerWithLabel.js
@@ -1,25 +1,25 @@
 L.CircleMarkerWithLabel = L.CircleMarker.extend({
     initialize: function (latlng, options) {
-		L.CircleMarker.prototype.initialize.call(this, latlng, options);
-		this._text = options.text;
-		var te = this._textElement = document.createElementNS('http://www.w3.org/2000/svg', "text");
-		te.textContent = this._text;
-		te.setAttribute("text-anchor","middle");
-	},
-	onAdd: function() {
+	L.CircleMarker.prototype.initialize.call(this, latlng, options);
+	this._text = options.text;
+	var te = this._textElement = document.createElementNS('http://www.w3.org/2000/svg', "text");
+	te.textContent = this._text;
+	te.setAttribute("text-anchor","middle");
+    },
+    onAdd: function() {
     	L.CircleMarker.prototype.onAdd.call(this);
     	this._redrawTextElement();
     	this._renderer._rootGroup.appendChild(this._textElement);
-	},
-	onRemove: function() {
+    },
+    onRemove: function() {
     	L.CircleMarker.prototype.onRemove.call(this);
     	this._textElement.remove();
-	},
-	_updatePath: function() {
+    },
+    _updatePath: function() {
     	L.CircleMarker.prototype._updatePath.call(this);
     	this._redrawTextElement();
-	},
-	_redrawTextElement: function() {
+    },
+    _redrawTextElement: function() {
     	this._textElement.textContent = this._text;
     	if (this._point) {
             this._textElement.setAttribute("class", "circle-with-marker-text");
@@ -29,7 +29,7 @@ L.CircleMarkerWithLabel = L.CircleMarker.extend({
             this._textElement.setAttribute("fill", this.options.textColor || "black");
             this._textElement.setAttribute("font-size",this.options.fontSize || this.getRadius());
         }
-	},
+    },
     setText: function(text) {
         this.options.text = this._text = text;
         return this.redraw();
@@ -41,7 +41,7 @@ L.CircleMarkerWithLabel = L.CircleMarker.extend({
 
 
 L.circleMarkerWithLabel = function(latlng, options) {
-	return new L.CircleMarkerWithLabel(latlng, options);
+    return new L.CircleMarkerWithLabel(latlng, options);
 };
 
 
